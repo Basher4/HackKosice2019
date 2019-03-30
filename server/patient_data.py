6ced3780785465b8d1b93eadef03e2e7d215363d
Matej Genči <matej.genci@gmail.com>
2019-03-30 16:35:36 +0000

    Add QOL feature

--- server/patient_data.py
+++ server/patient_data.py
@@ -4,6 +4,12 @@ def PatientFromJson(data, appointment_time):
                        appointment_time,
                        data["travel_time"])
 
+def PatientFromJson(data):
+    return PatientData(data["id"],
+                       data["email"],
+                       -1,
+                       data["travel_time"])
+
 class PatientData():
     def __init__(self, id, email, appointment_time, travel_time):
         self.id = id
