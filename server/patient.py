d470f8bfe4e00d32de234072dfc71eb080c395fa
Sara <sara.kutkova@gmail.com>
2019-03-30 22:32:17 +0100

    sara masterpiece

--- server/patient.py
+++ server/patient.py
@@ -1,14 +1,21 @@
 from flask import jsonify
 import patient_data
+import datetime
 
 def enqueue(hkqueue, data):
-    print(data)
+    if (len(hkqueue.timeline) == 0) or (hkqueue.timeline[hkqueue.end_index].appointment_time > hkqueue.avg_examination_time + 10 + int(datetime.datetime.now()[15:17])*60 + int(datetime.datetime.now()[18:20])):
+        if (int(datetime.datetime.now()[15:17])*60 + int(datetime.datetime.now()[18:20])) < hkqueue.start_time:
+            data.appointment_time = hkqueue.start_time
+        else:
+            data.appointment_time = int(datetime.datetime.now()[15:17])*60 + int(datetime.datetime.now()[18:20])
+    else:
+        data.appointment_time = hkqueue.timeline[hkqueue.end_index].appointment_time
+    stav = hkqueue.get_first_free_slot()
     resp = {
-        "full": False,
-        "pos_in_queue": data.id,
-        "waiting_time": 20
+        "full": stav,
+        "pos_in_queue": str(len(hkqueue.timeline)),
+        "waiting_time": str(len(hkqueue.timeline))*hkqueue.avg_examination_time
     }
-
     return jsonify(resp)
 
 def cancel(hkqueue, data):
--- server/patient_data.py
+++ server/patient_data.py
@@ -11,8 +11,6 @@ class PatientData():
     def __init__(self, id, email, appointment_time, travel_time):
         self.id = id
         self.email = email
-        self.appointment_time = appointment_time
+        self.appointment_time = HkQueue.timeline[HkQueue.end_index].appointment_time + 1
         self.travel_time = travel_time
-
-    def offset_appointment(self, deltaT):
-        self.appointment_time += deltaT
+        self.appointment_time = appointment_time
