c03bb385c5c3b6e0460109b303435bd18f3839d3
Lukacma <marian.lukac18@imperial.ac.uk>
2019-03-30 21:39:28 +0000

    Wiating for server

--- mobil/App5/App5/App5/Uvodna.xaml.cs
+++ mobil/App5/App5/App5/Uvodna.xaml.cs
@@ -1,21 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Xamarin.Forms;
+using System;
+using System.IO;
+using Org.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
-
-using Xamarin.Forms;
+using Newtonsoft.Json;
 using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
 
 namespace App5
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Uvodna : ContentPage
 	{
-		public Uvodna ()
+        Dictionary<string, string> info;
+
+         public  Uvodna(string str)
 		{
 			InitializeComponent ();
-		}
+
+            //info = JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
+            //pocet.Text = info["pos_in_queue"];
+            //cas.Text = info["waiting_time"];
+            //if (info["full"] == "True")
+            //{
+            //    objednaj.Text = "Plne";
+            //    objednaj.IsEnabled = false;
+           
+            //}
+        }
+        
         async void OnSendButtonClicked(object sender, EventArgs e)
         {
 
