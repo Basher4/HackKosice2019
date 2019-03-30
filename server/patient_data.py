16e57a3d6586e3d11082dab44fa97a6aa0782c16
Matej Genči <matej.genci@gmail.com>
2019-03-30 16:30:20 +0000

    More data

--- server/patient_data.py
+++ server/patient_data.py
@@ -1,11 +1,15 @@
 def PatientFromJson(data, appointment_time):
-    return PatientData(data["id"], data["email"], appointment_time)
+    return PatientData(data["id"],
+                       data["email"],
+                       appointment_time,
+                       data["travel_time"])
 
 class PatientData():
-    def __init__(self, id, email, appointment_time):
+    def __init__(self, id, email, appointment_time, travel_time):
         self.id = id
         self.email = email
         self.appointment_time = appointment_time
+        self.travel_time = travel_time
 
     def offset_appointment(self, deltaT):
         self.appointment_time += deltaT
