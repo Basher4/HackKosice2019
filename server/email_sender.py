6e23a660b903a4720983319ad9a20f89fc68ffd1
Matej Genči <matej.genci@gmail.com>
2019-03-30 21:20:03 +0000

    Dummy stats data

---
+++ server/email_sender.py
@@ -0,0 +1,28 @@
+import smtplib
+import patient_data
+
+def schedule_email(user):
+    when_to_send_email = user.appointment_time
+
+def send_email(user):
+    sender = 'hackkosice2019cakaren@gmail.com'
+    receivers = [user.email]
+
+    message = """From: Totally Awesome Project <hackkosice2019cakaren@gmail.com>
+    To: Totally Awesome Person <{}>
+    Hey, you! You have an appointment at {}th minute of this day! Get yo' ass here.
+    """.format(user.email, user.appointment_time)
+
+    print(message)
+
+    smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    smtpObj.login("hackkosice2019cakaren", "hackkosice")
+    smtpObj.sendmail(sender, receivers, message)         
+    smtpObj.quit()
+    print("Email sent to " + user.email)
+
+if __name__ == "__main__":
+    ja = patient_data.PatientData(123, 'matej.genci@gmail.com', 1600, 30)
+    ty = patient_data.PatientData(321, 'sara.kutkova@gmail.com', 1200, 24)
+    send_email(ja)
+    send_email(ty)
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import patient, doctor, hkqueue
+import patient, doctor, hkqueue, stats
 import patient_data as pd
 import json
 app = Flask(__name__)
@@ -36,9 +36,9 @@ def doctor_entered():
     return doctor.patient_entered(app.config["hkqueue"],
                                     pd.PatientFromJson1(data))
 
-@app.route("/api/stats", methods = ["POST", "GET"])
-def stats():
-    return jsonify({"response":"idk"})
+@app.route("/api/stats/status", methods = ["POST", "GET"])
+def stats_status():
+    return stats.now_waiting(app.config["hkqueue"])
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
