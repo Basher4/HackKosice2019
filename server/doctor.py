d6d7fb1d2437e6536744e87494b6f966fae69022
Sara <sara.kutkova@gmail.com>
2019-03-31 09:04:06 +0200

    doctor_get_funguje

--- server/doctor.py
+++ server/doctor.py
@@ -11,6 +11,7 @@ def patient_entered(hkqueue):
     if len(hkqueue.timeline) == 0:
         return jsonify()
     pat_now = hkqueue.timeline.pop(0)
+    hkqueue.end_index -= 1
     if len(hkqueue.timeline) > 0:
         time_ap = hkqueue.timeline[0].appointment_time
         time_now = datetime.now().hour * 60 + datetime.now().minute
@@ -18,4 +19,10 @@ def patient_entered(hkqueue):
         for i in range(hkqueue.end_index):
             print(hkqueue.end_index)
             hkqueue.timeline[i].offset_appointment(change) # reschedules email
-    return jsonify(pat_now)
+    new_pat_now = {
+            "id": pat_now.id,
+            "email": pat_now.email,
+            "typ": pat_now.type,
+            "appointment_time": pat_now.appointment_time,
+            "travel_time": pat_now.travel_time}
+    return jsonify(new_pat_now)
