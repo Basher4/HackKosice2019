0174a1558a914889b5c8d4ebca8c3736a2c9adf5
Sara <sara.kutkova@gmail.com>
2019-03-31 03:06:10 +0200

    lala

--- server/hkqueue.py
+++ server/hkqueue.py
@@ -9,26 +9,21 @@ class HkQueue(object):
         self.end_index = 0
     
     def get_first_free_slot(self): #fix_first_free_slot
-        print()
-        print("end_index", self.end_index)
-        print("timeline", len(self.timeline))
-        print()
-
         if self.end_index == len(self.timeline) - 1:
             if self.timeline[self.end_index].appointment_time + self.avg_examination_time + 1 < self.end_time:
                 self.end_index += 1
-                return True
-            else:
                 return False
+            else:
+                return True
         else:
             if self.timeline[self.end_index].appointment_time + self.avg_examination_time <= self.timeline[self.end_index + 1].appointment_time:
                 self.end_index += 1
-                return True
+                return False
             else:
                 self.timeline[self.end_index + 1].appointment_time = self.timeline[self.end_index].appointment_time + self.avg_examination_time + 2 # maybe len +1
                 self.end_index += 2
                 self.get_first_free_slot()
-                return True
+                return False
 
     def add_patient(self, patient):
         self.timeline.append(patient)
